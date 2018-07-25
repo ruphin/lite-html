ce5ab61f0b205833f6ac10526d220680c0a6bfdb
Goffert van Gool <ruphin@ruphin.net>
2018-07-25 03:50:26 +0200

    It works™

--- src/html-lite.js
+++ src/html-lite.js
@@ -23,6 +23,8 @@
  * SOFTWARE.
  */
 
+const lastAttributeNameRegex = /[ \x09\x0a\x0c\x0d]([^\0-\x1F\x7F-\x9F \x09\x0a\x0c\x0d"'>=/]+)[ \x09\x0a\x0c\x0d]*=[ \x09\x0a\x0c\x0d]*(?:[^ \x09\x0a\x0c\x0d"'`<>=]*|"[^"]*|'[^']*)$/;
+
 const marker = Math.random()
   .toString(36)
   .substring(2);
@@ -33,16 +35,21 @@ const attributeContext = Symbol('attributeContext');
 const unchangedContext = Symbol('unchangedContext');
 
 // TODO: Create constants to compare comment contents
-const commentMarker = `--><!--html-comment-${marker}--><!--`;
-const nodeMarker = `<!--html-node-${marker}-->`;
-const attributeMarker = `html-attribute-${marker}`;
+const commentMarker = `html-comment-${marker}`;
+const commentMarkerTag = `--><!--${commentMarker}--><!-- `;
+const nodeMarker = `html-node-${marker}`;
+const nodeMarkerTag = `<!--${nodeMarker}-->`;
+const attributeMarker = `__html-attribute-${marker}`;
+const attributeMarkerTag = `${attributeMarker} ${attributeMarker}`;
 
 const contextMap = new Map();
-contextMap.set(commentContext, commentMarker);
-contextMap.set(nodeContext, nodeMarker);
-contextMap.set(attributeContext, attributeMarker);
+contextMap.set(commentContext, commentMarkerTag);
+contextMap.set(nodeContext, nodeMarkerTag);
+contextMap.set(attributeContext, attributeMarkerTag);
 
 const templateMap = new Map();
+
+const isPrimitive = value => !(typeof value === 'object' || typeof value === 'function');
 
 const htmlContext = htmlString => {
   const openComment = htmlString.lastIndexOf('<!--');
@@ -52,7 +59,7 @@ const htmlContext = htmlString => {
   if (openComment > -1 && !commentClosed) {
     context = commentContext;
   } else {
-    const closeTag = htmlString.lastIndexOf('>', closeComment);
+    const closeTag = htmlString.lastIndexOf('>', htmlString.length - closeComment);
     const openTag = htmlString.indexOf('<', closeTag + 1);
     if (openTag > -1) {
       context = attributeContext;
@@ -67,38 +74,125 @@ const htmlContext = htmlString => {
   return { commentClosed, context };
 };
 
-const walkTree = (element, path) => {
-  let parts = [];
-  // Comment Node
-  if (element.nodeType === 8) {
-    if (element.nodeValue === commentMarker) {
-      parts.push(new PartLocation(commentPart, path)); // TODO: Construct Part directly (I think)
-    } else if (element.nodeValue === nodeMarker) {
-      parts.push(new PartLocation(nodePart, path));
+class Part {
+  constructor(path, instance) {
+    this.path = path;
+    this.previousValue = undefined;
+  }
+
+  clone() {
+    return Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+  }
+}
+
+class NodePart extends Part {
+  set node(node) {
+    this.previousNode = node;
+    this.startMarker = document.createComment('');
+    this.endMarker = document.createComment('');
+    node.parentNode.insertBefore(this.startMarker, node);
+    node.parentNode.insertBefore(this.endMarker, node);
+  }
+  update(value) {
+    if (isPrimitive(value)) {
+      // Handle primitive values
+      // If the value didn't change, do nothing
+      if (value === this.previousValue) {
+        return;
+      }
+      this._setText(value);
+    } else if (value instanceof TemplateResult) {
+      this._setTemplateResult(value);
+      // TODO: Arrays?
+      // } else if (Array.isArray(value) || value[Symbol.iterator]) {
+      //   this._setIterable(value);
+    } else if (value instanceof Node) {
+      this._setNode(value);
+    } else if (value.then !== undefined) {
+      this._setPromise(value);
+    } else {
+      // Fallback, will render the string representation
+      this._setText(value);
     }
-    return parts;
+
+    // TODO: Add other nodes than textnodes.
   }
 
-  // TODO: Detect other node types
-
-  const children = element.childNodes;
-  const length = children.length;
-  for (let i = 0; i < length; i++) {
-    parts = parts.concat(walkTree(children[i]), path.concat([i]));
+  _setText(text) {
+    if (this.previousValue === text) {
+      return;
+    }
+    if (this.endMarker.previousSibling.nodeType === 3) {
+      this.endMarker.previousSibling.nodeValue = text;
+    } else {
+      this._setNode(document.createTextNode(text));
+    }
   }
-};
 
-// TODO: Create Parts directly
-class PartLocation {
-  constructor(nodeType, position) {
-    this.nodeType = nodeType;
-    this.position = position;
+  _setNode(node) {
+    if (this.previousNode === node) {
+      return;
+    }
+    const parent = this.endMarker.parentNode;
+    if (this.previousNode instanceof DocumentFragment) {
+      while (this.endMarker.previousSibling != this.startMarker) {
+        this.previousNode.appendChild(this.endMarker.previousSibling);
+      }
+    } else {
+      while (this.endMarker.previousSibling != this.startMarker) {
+        parent.removeChild(this.endMarker.previousSibling);
+      }
+    }
+
+    parent.insertBefore(node, this.endMarker);
+    this.previousNode = node;
+  }
+
+  _setTemplateResult(templateResult) {
+    let instance = this.instance.subTemplates.get(templateResult.template);
+    if (!instance) {
+      instance = new TemplateInstance(templateResult.template);
+      this.instance.subTemplates.set(templateResult.template, instance);
+    }
+    if (instance.fragment !== this.previousNode) {
+      this._setNode(instance.fragment);
+    }
+    instance.update(templateResult.values);
+  }
+}
+
+class CommentPart extends Part {
+  set node(node) {
+    this._node = node;
+    this.nextSibling = node.nextSibling;
+    this.parentNode = node.parentNode;
+  }
+  update(value) {
+    const newNode = document.createComment(value);
+    const parentNode = this._node.parentNode;
+    parentNode.insertBefore(newNode, this.nextSibling);
+    parentNode.removeChild(this._node);
+    this._node = newNode;
+  }
+}
+
+class AttributePart extends Part {
+  constructor(path, attribute) {
+    super(path);
+    this.attribute = attribute;
+  }
+  update(value) {
+    this.node.setAttribute(this.attribute, value);
   }
 }
 
 class Template {
   constructor(strings) {
     this.strings = strings;
+    this.parts = [];
+    this.element = document.createElement('template');
+    this.element.innerHTML = this.HTML;
+    this.createParts(this.element.content, []);
   }
 
   get HTML() {
@@ -115,23 +209,48 @@ class Template {
       html.push(contextMap.get(currentContext));
     }
     html.push(this.strings[lastStringIndex]);
-
     return html.join('');
   }
 
-  get element() {
-    const element = document.createElement('template');
-    element.innerHTML = this.HTML;
-    Object.defineProperty(this, 'element', {
-      get() {
-        return element;
+  createParts(node, path) {
+    if (node.nodeType === 8) {
+      if (node.nodeValue === commentMarker) {
+        this.parts.push(new CommentPart(path));
+      } else if (node.nodeValue === nodeMarker) {
+        this.parts.push(new NodePart(path));
       }
-    });
-    return element;
-  }
+      return;
+    }
 
-  get parts() {
-    const template = this.element.cloneNode(true);
+    // Element Node
+    if (node.nodeType === 1) {
+      if (node.hasAttribute(attributeMarker)) {
+        node.removeAttribute(attributeMarker);
+
+        const attributes = Array.from(node.attributes);
+        const dynamicAttributes = attributes.filter(attribute => attribute.value === attributeMarker);
+
+        for (let i = 0; i < dynamicAttributes.length; i++) {
+          const attributeMatch = lastAttributeNameRegex.exec(this.strings[this.parts.length]);
+          if (attributeMatch) {
+            const attribute = attributeMatch[1];
+            this.parts.push(new AttributePart(path, attribute));
+          } else {
+            throw new Error(`Invalid dynamic: ${this.strings[this.parts.length]}\${ !! }${this.strings[this.parts.length + 1]}`);
+          }
+        }
+      }
+    }
+
+    // TODO: Attribute existence parts?
+
+    // TODO: Template nodes?
+
+    const children = node.childNodes;
+    const length = children.length;
+    for (let i = 0; i < length; i++) {
+      this.createParts(children[i], path.concat([i]));
+    }
   }
 }
 
@@ -154,14 +273,32 @@ class TemplateResult {
 class TemplateInstance {
   constructor(template) {
     this.template = template;
+    this.subTemplates = new Map();
+    this.parts = [];
+    template.parts.forEach(part => {
+      const instancePart = part.clone();
+      instancePart.instance = this;
+      this.parts.push(instancePart);
+    });
+    this.fragment = template.element.content.cloneNode(true);
+    this.initializeParts();
   }
 
-  clone() {
-    const fragment = this.template.element.content.cloneNode(true);
-    return fragment;
+  initializeParts() {
+    // Attach parts to nodes in this.fragment
+    this.parts.forEach(part => {
+      let node = this.fragment;
+      console.log(part.path);
+      console.log('NODE', node);
+      part.path.forEach(nodeIndex => {
+        node = node.childNodes[nodeIndex];
+        console.log(node);
+      });
+      part.node = node;
+    });
   }
   update(values) {
-    // Do things with parts
+    this.parts.map((part, index) => part.update(values[index]));
   }
 }
 
@@ -186,10 +323,10 @@ export const render = (templateResult, target) => {
     instance = new TemplateInstance(templateResult.template);
     target.__templateInstance = instance;
 
-    const fragment = instance.clone();
     removeNodes(target, target.firstChild);
-    target.appendChild(fragment);
+    instance.update(templateResult.values);
+    target.appendChild(instance.fragment);
+  } else {
+    instance.update(templateResult.values);
   }
-
-  instance.update(templateResult.values);
 };
