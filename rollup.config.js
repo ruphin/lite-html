8e07422846c8af95106773ee6782238ad9c6e418
Goffert van Gool <ruphin@ruphin.net>
2018-07-29 03:04:29 +0200

    Correct licensing in build output

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,39 @@
 import filesize from 'rollup-plugin-filesize';
-import uglify from 'rollup-plugin-uglify';
+import cleanup from 'rollup-plugin-cleanup';
+import { terser } from 'rollup-plugin-terser';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import * as path from 'path';
+
+const license = `/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2018 Goffert van Gool
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+`;
 
 function getConfig({ dest, format, uglified = false, transpiled = false }) {
   const conf = {
     input: 'src/html-lite.js',
-    output: { file: dest, name: 'html-lite', format, sourcemap: true },
+    output: { banner: license, file: dest, name: 'html-lite', format, sourcemap: true },
     plugins: [
       transpiled &&
         commonjs({
@@ -18,15 +44,13 @@ function getConfig({ dest, format, uglified = false, transpiled = false }) {
           presets: [['env', { modules: false }]],
           plugins: ['external-helpers']
         }),
-      uglified &&
-        uglify({
-          warnings: true,
-          toplevel: uglified,
-          sourceMap: uglified,
-          compress: uglified && { passes: 2 },
-          mangle: uglified,
-          output: { beautify: !uglified }
+      !uglified &&
+        cleanup({
+          maxEmptyLines: 1,
+          // This removes redundant @license comments
+          comments: [/^[\*\s]*[^@\*\s]/]
         }),
+      uglified && terser({ warnings: true, output: { preamble: license } }),
       filesize()
     ].filter(Boolean)
   };
