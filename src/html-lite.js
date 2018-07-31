46cc698c8d9fef65ee543ee4d5aa2981176ab34b
Goffert van Gool <ruphin@ruphin.net>
2018-07-31 23:04:08 +0200

    Start with documentation

--- src/html-lite.js
+++ src/html-lite.js
@@ -26,15 +26,30 @@
 import { TemplateResult } from './lib/templates.js';
 import { NodePart } from './lib/parts.js';
 
+/**
+ * Tagging function to tag JavaScript template string literals as HTML
+ */
 export const html = (strings, ...values) => {
   return new TemplateResult(strings, values);
 };
 
-export const render = (templateResult, target) => {
+/**
+ * Render content into a target node
+ *
+ * @param {any} content
+ *   Any content you wish to render. Usually a template string literal tagged with the `html` function
+ * @param {Node} target
+ *   An HTML Node that you wish to render the content into.
+ *   The content will become the sole content of the target node.
+ */
+export const render = (content, target) => {
+  // Check if the target has a NodePart that represents its content
   let part = target.__nodePart;
   if (!part) {
+    // If it does not, create a new NodePart
     part = new NodePart(undefined, target);
     target.__nodePart = part;
   }
-  part.render(templateResult);
+  // Task the NodePart of this target to render the content
+  part.render(content);
 };
